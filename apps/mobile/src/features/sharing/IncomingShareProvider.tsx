01f401ab81b944c92446404f5c3a4da0cf009a4c
Theo Browne <me@t3.gg>
2026-07-30 01:48:16 -0700

    fix(mobile): stop shared content errors in Personal Team builds (#4943)
    (cherry picked from commit 00da6b57a43aef39162ff9e8205b5441c24298d0)

--- apps/mobile/src/features/sharing/IncomingShareProvider.tsx
+++ apps/mobile/src/features/sharing/IncomingShareProvider.tsx
@@ -16,6 +16,7 @@ import {
   type IncomingShareDestination,
   type IncomingShareDraft,
 } from "./incoming-share-model";
+import { createIncomingSharePayloadReader } from "./incoming-share-native";
 import { IncomingShareInbox } from "./incoming-share-inbox";
 import {
   loadIncomingShareDrafts,
@@ -50,6 +51,11 @@ function receiveSharingEnabled(): boolean {
   }
   return Constants.expoConfig?.extra?.iosPersonalTeamBuild !== true;
 }
+
+const getIncomingSharePayloads = createIncomingSharePayloadReader({
+  platform: Platform.OS,
+  readPayloads: getSharedPayloads,
+});
 
 async function resolvedPayloadsForImages(): Promise<ReadonlyArray<ResolvedSharePayload>> {
   try {
@@ -124,7 +130,7 @@ const incomingShareInbox = new IncomingShareInbox({
   loadDrafts: loadIncomingShareDrafts,
   writeDraft: writeIncomingShareDraft,
   removeDraft: removeIncomingShareDraft,
-  getPayloads: getSharedPayloads,
+  getPayloads: getIncomingSharePayloads,
   clearPayloads: clearSharedPayloads,
   buildDraft: async ({ payloads, id, createdAt }) => {
     const cleanupUris = new Set<string>();
